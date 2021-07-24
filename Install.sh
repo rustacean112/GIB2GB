7e9361dba0dae7cae761733d652d4824328be8a3
Linux fan also loves Windows <63163893+Huso112@users.noreply.github.com>
2021-07-24 20:09:24 +0000

    Update Install.sh

--- Install.sh
+++ Install.sh
@@ -1,4 +1,4 @@
 mkdir ~/.gib2gb
 cp -r ~/GIB2GB/GİB2GB.sh $HOME/.gib2gb
-echo " " >> ~/.bashrc && echo "#======GIB2GB======" >> ~/.bashrc && echo 'alias gib2gb="sh ~/.gib2gb/GİB2GB.sh"' >> ~/.bashrc && echo 'alias gib2gb-help="xdg-open https://github.com/Huso112/GIB2GB/blob/main/README.md"' >> ~/.bashrc
+echo " " >> ~/.bashrc && echo "#======GIB2GB======" >> ~/.bashrc && echo 'alias gib2gb="sh ~/.gib2gb/GİB2GB.sh"' >> ~/.bashrc && echo 'alias gib2gb-help="xdg-open https://github.com/Huso112/GIB2GB/blob/main/README.md"' >> ~/.bashrc && echo 'alias gib2gb="sh ~/.gib2gb/GİB2GB-cdu.sh"' >> ~/.bashrc
 echo "GIB2GB has successfully installed." 
